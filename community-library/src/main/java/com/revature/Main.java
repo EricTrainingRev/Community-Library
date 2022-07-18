ddc1db6cae2173cc5b1a76d34a53aafe635ae125
EricTrainingRev <eric.suminski@revature.com>
2022-07-18 15:47:41 -0400

    finished first round of API

--- community-library/src/main/java/com/revature/Main.java
+++ community-library/src/main/java/com/revature/Main.java
@@ -1,6 +1,11 @@
 package com.revature;
 
 import com.revature.controllers.BookController;
+import com.revature.repository.BookDAO;
+import com.revature.repository.BookDAOInterface;
+import com.revature.service.BookService;
+import com.revature.service.BookServiceInterface;
+import com.revature.utils.BusinessRules;
 
 import io.javalin.Javalin;
 
@@ -15,10 +20,33 @@ public class Main {
             config.enableDevLogging();
         });
 
-        BookController bookController = new BookController();
+        /*
+            For our controller class to be able to send information to the service layer for validation it
+            must be given a service object. However, that service object requires its own dependencies to work
+            correctly, so this is where we create our official objects for our repository and service layers so
+            that our API can correctly send information where it needs to go
+        */
+
+        BookDAOInterface bookDao = new BookDAO();
+        BusinessRules businessRules = new BusinessRules();
+        BookServiceInterface bookService = new BookService(bookDao, businessRules);
+        BookController bookController = new BookController(bookService);
 
 
         app.get("/hello", bookController.getHelloWorld);
+
+        /*
+            notice that my path strings all include book in them: this is part of creating a restful web service, something we will talk
+            more about tomorrow
+        */
+
+        app.get("/book", bookController.getAllBooks);
+
+        app.delete("/book", bookController.deleteBook);
+
+        app.patch("/book", bookController.updateBook);
+
+        app.post("/book", bookController.createBook);
 
         app.start();
 
